499524445bca427610ad8add7cf00517c860817a
Maxime Tricoire <maxime.tricoire@datathings.com>
2026-04-09 13:38:35 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite-plus";
-import greycat from '@greycat/web/vite-plugin';
+import greycat from "@greycat/web/vite-plugin";
 
 export default defineConfig({
   staged: {
