7d7e1da1ee74dd887006b1d0176586c2c6e801a9
Maxime Tricoire <maxime.tricoire@datathings.com>
2026-04-09 12:24:59 +0200

    Initial template

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import '@greycat/web';
+import '@greycat/web/greycat.css';
+import './index.css';
+
+const greycat = await gc.sdk.init({ debug: import.meta.env.DEV });
+
+const root = await greycat.root();
+const value = await root['project::count'].resolve() ?? 0;
+
+const count = document.createTextNode(`${value}`);
+
+async function inc() {
+  const value = await gc.project.inc();
+  count.textContent = `${value}`;
+}
+
+async function dec() {
+  const value = await gc.project.dec();
+  count.textContent = `${value}`;
+}
+
+document.body.appendChild(
+  <main>
+    <div className="count-value">{count}</div>
+    <div className="count-label">Count</div>
+    <div className="count-actions">
+      <sl-button onclick={dec}>-</sl-button>
+      <sl-button onclick={inc}>+</sl-button>
+    </div>
+  </main>,
+);
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite-plus";
+import greycat from '@greycat/web/vite-plugin';
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [greycat()],
+});
